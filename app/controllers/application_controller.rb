e80f165f055b7e44a793958dd06d0d27152a68a8
StevenDessasore <stevendessasoreba@gmail.com>
2021-08-17 03:20:39 -0400

    commit tree 7bcf984b2ae70a2ff0fadbb921930ce44c1d9f4e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < Sinatra::Base
 
-  configure do
-  	set :views, "app/views"
-  	set :public_dir, "public"
+  configure do 
+    set :views, "app/views"
+    set :public_dir, "public"
   end
 
   get "/" do
-  	erb :index
+    erb :index
   end
+  
 end
